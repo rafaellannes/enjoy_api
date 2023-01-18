bf9ff4dccb3bd0f00c07e393c2048b42a1ae3a3a
Rafael Lannes <rafaelvascolannes@gmail.com>
2023-01-18 15:16:47 -0300

    tradução api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +38,13 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'message' => __('validation.headermsg'),
+            'errors' => $exception->errors(),
+        ], $exception->status);
     }
 }
