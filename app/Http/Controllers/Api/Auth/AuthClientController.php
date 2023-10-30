8475c4219cc2662d6a843183393baba66c8e5c00
rhyltonn <rhylton.figueiredo@gmail.com>
2023-10-30 14:46:30 -0300

    Restição do 403 só para e-mail (TESTE)

--- app/Http/Controllers/Api/Auth/AuthClientController.php
+++ app/Http/Controllers/Api/Auth/AuthClientController.php
@@ -22,7 +22,7 @@ class AuthClientController extends Controller
 
         $client = Client::where('email', $request->email)->first();
 
-        if($request->plataform == 'Email'){
+        if($request->plataform == 'email'){
             if ($client && $client->plataforma != $request->plataforma) {
                 return response()->json([
                     'message' => 'Você se cadastrou pelo '.$client->plataforma.', faça o login por ela.',
